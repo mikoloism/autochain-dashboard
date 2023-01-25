fba5ae47423047122cb7be4963fefcbcbf68422b
mikoloism <mikoloism@gmail.com>
2023-01-25 18:05:46 +0330

    feat(libs): add `normalize.css` to the `app` renderer;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
+import '@/libs/normalize.css';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component: PageComponent, pageProps }: AppProps) {
+function AppRenderer({ Component: PageComponent, pageProps }: AppProps) {
 	return <PageComponent {...pageProps} />;
 }
+
+export default AppRenderer;
